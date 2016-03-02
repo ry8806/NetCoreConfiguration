29030b887f52c549dcc087ac8aec8dcc3a8a9add
Ryan Southgate <ryansouthgate@live.com>
2016-03-02 22:27:10 +0000

    added initial examples

---
+++ NetCoreConfiguration/src/NetCoreConfiguration/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace NetCoreConfiguration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var environment = "development";
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environment}.json", optional: true);
+            var configuration = builder.Build();
+
+            // Is "1.0.0" (from appsettings.json)
+            string version = configuration["Version"];
+
+            // Is "test-key" (from appsettings.development.json)
+            string apiKey = configuration["ApiKey"];
+
+            // Is "http://test.myapi.com/" (from appsettings.development.json)
+            string apiAddress = configuration["ApiAddress"];
+
+            // Is ["Steve", "Lucy", "Harry"] (from appsettings.json)
+            var users = configuration.GetSection("Auth:Users").GetChildren().Select(x => x.Value);
+
+            ///// ------------- From appsettings.json ------------ /////   /// From appsettings.development.json ///
+            // Is ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",          "Saturday", "Sunday"]
+            var availableDays = configuration.GetSection("ServerAvailable:Days").GetChildren().Select(x => x.Value);
+
+            // Is [12] (from appsettings.development.json)
+            var updateAtHours = configuration.GetSection("UpdatesOn:Hours").GetChildren().Select(x => x.Value);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NetCoreConfiguration/src/NetCoreConfiguration/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NetCoreConfiguration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetCoreConfiguration")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0fd1c308-6444-4722-8554-b0d7383f7e4b")]
